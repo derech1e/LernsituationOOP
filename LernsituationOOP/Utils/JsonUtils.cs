9724dc99107435910f008d37aba759428b51f43d
Thomas Nürk <jojopaul88@gmail.com>
2020-12-09 21:03:50 +0100

    Comments & Parms rewritten for better understanding

--- LernsituationOOP/Utils/JsonUtils.cs
+++ LernsituationOOP/Utils/JsonUtils.cs
@@ -8,59 +8,59 @@ namespace LernsituationOOP.Utils
 {
     public class JsonUtils
     {
-        /// <summary>Speichert das gegebene Objekt am gegebenem Dateipfad</summary>
+        /// <summary>Speichert das gegebene Objekt (in diesem Fall ein String) am gegebenem Dateipfad ab</summary>
         /// <param name="filePath">Dateipfad</param>
-        /// <param name="textToSave">Text der zu speichern ist</param>
-        private static void SaveJsonToFile(string filePath, object textToSave)
+        /// <param name="objectToJson">Text der zu speichern ist</param>
+        private static void SerializeObjectToFile(string filePath, object objectToJson)
         {
-            string contents = JsonConvert.SerializeObject(textToSave);
-            File.WriteAllText(filePath, contents);
+            string jsonString = JsonConvert.SerializeObject(objectToJson);
+            File.WriteAllText(filePath, jsonString);
         }
 
-        /// <summary>Liest den Json aus der angegebenen Datei aus</summary>
+        /// <summary>Liest den Inhalt (Text) aus der angegebenen Datei aus</summary>
         /// <param name="filePath">Dateipfad</param>
-        /// <returns>Gibt den Erfolg der Auslesen zurück</returns>
-        private static string LoadJsonFromFile(string filePath) => !File.Exists(filePath) ? "" : File.ReadAllText(filePath);
+        /// <returns>Gibt den Erfolg des Auslesen zurück</returns>
+        private static string ReadTextFromFile(string filePath) => !File.Exists(filePath) ? "" : File.ReadAllText(filePath);
 
         /// <summary>Speichert alle Reservierungen</summary>
-        /// <returns>Gibt den Erfolg der Speicherung zurück</returns>
+        /// <returns>Gibt den Erfolg des Speichern zurück</returns>
         public static bool SaveReservierungen()
         {
-            JsonUtils.SaveJsonToFile(Utils.FILE_PATH_RESERVIERUNGEN, Utils.Reservierungen);
+            SerializeObjectToFile(Utils.FILE_PATH_RESERVIERUNGEN, Utils.Reservierungen);
             return true;
         }
 
         /// <summary>Speichert alle Mitarbeiter</summary>
-        /// <returns>Gibt den Erfolg der Speicherung zurück</returns>
+        /// <returns>Gibt den Erfolg des Speichern zurück</returns>
         public static bool SaveMitarbeiter()
         {
-            JsonUtils.SaveJsonToFile(Utils.FILE_PATH_MITARBEITER, Utils.Mitarbeiter);
+            SerializeObjectToFile(Utils.FILE_PATH_MITARBEITER, Utils.Mitarbeiter);
             return true;
         }
 
         /// <summary>Liest alle Reservierungen aus der Datei aus</summary>
-        /// <returns>Gibt den Erfolg der Auslesen zurück</returns>
+        /// <returns>Gibt den Erfolg des Auslesen zurück</returns>
         public static bool LoadReservierungen()
         {
-            List<Reservierung> reservierungen = JsonConvert.DeserializeObject<List<Reservierung>>(LoadJsonFromFile(Utils.FILE_PATH_RESERVIERUNGEN));
+            List<Reservierung> reservierungen = JsonConvert.DeserializeObject<List<Reservierung>>(ReadTextFromFile(Utils.FILE_PATH_RESERVIERUNGEN));
             Utils.Reservierungen = reservierungen ?? new List<Reservierung>();
             return true;
         }
 
         /// <summary>Liest alle Reservierungen aus der Datei aus</summary>
-        /// <returns>Gibt den Erfolg der Auslesen zurück</returns>
+        /// <returns>Gibt den Erfolg des Auslesen zurück</returns>
         public static bool LoadReservierungen1()
         {
-            List<Reservierung> reservierungen = JsonConvert.DeserializeObject<List<Reservierung>>(LoadJsonFromFile(Utils.FILE_PATH_RESERVIERUNGEN));
+            List<Reservierung> reservierungen = JsonConvert.DeserializeObject<List<Reservierung>>(ReadTextFromFile(Utils.FILE_PATH_RESERVIERUNGEN));
             Utils.Reservierungen = reservierungen ?? new List<Reservierung>();
             return true;
         }
 
         /// <summary>Liest alle Mitarbeiter aus der Datei aus</summary>
-        /// <returns>Gibt den Erfolg der Auslesen zurück</returns>
+        /// <returns>Gibt den Erfolg des Auslesen zurück</returns>
         public static bool LoadMitarbeiter()
         {
-            List<Mitarbeiter> mitarbeiter = JsonConvert.DeserializeObject<List<Mitarbeiter>>(LoadJsonFromFile(Utils.FILE_PATH_MITARBEITER));
+            List<Mitarbeiter> mitarbeiter = JsonConvert.DeserializeObject<List<Mitarbeiter>>(ReadTextFromFile(Utils.FILE_PATH_MITARBEITER));
             Utils.Mitarbeiter = mitarbeiter ?? new List<Mitarbeiter>();
             return true;
         }
